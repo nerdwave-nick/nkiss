069a580444afaf13df539fada8de5fa7e9dd8f78
nerdwave-nick <nick@nerdwave.io>
2025-12-01 22:58:23 +0100

    feat: update printing

--- packages/hardware_packages.py
+++ packages/hardware_packages.py
@@ -4,9 +4,26 @@ import subprocess
 import re
 import sys
 
+RESET = '\033[0m'
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+DGREY = '\033[2;37m'
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def print_e(string):
+    eprint(f"{RESET}{RED}:: {string}{RESET}")
+
+
+def print_g(string):
+    eprint(f"{RESET}{DGREY}:: {string}{RESET}")
+
+
+def print_s(string):
+    eprint(f"{RESET}{GREEN}:: {string}{RESET}")
 
 
 # base packages for any system
@@ -47,27 +64,29 @@ class HardwareDetector:
         # -d ::03xx : Class 03 (Display Controller)
         out = self._run_cmd(["lspci", "-n", "-d", "::03xx"])
         if not out:
-            eprint("Failed to run lspci")
+            print_e("Failed to run lspci")
 
         for line in out.splitlines():
             # Format: "00:02.0 0300: 8086:3e92 (rev 00)"
             match = re.search(r'\s([0-9a-fA-F]{4}):([0-9a-fA-F]{4})', line)
             if not match:
-                eprint("Failed to parse lspci output")
+                print_e("Failed to parse lspci output")
                 continue
 
             vendor = match.group(1).lower()
             device = match.group(2).lower()
 
             if vendor == "8086":   # INTEL
-                eprint("detected intel gpu")
+                print_g("detected intel gpu")
                 self._handle_intel(device, pkgs)
             elif vendor == "10de":  # NVIDIA
-                eprint("detected nvidia gpu")
+                print_g("detected nvidia gpu")
                 self._handle_nvidia(device, pkgs)
             elif vendor == "1002":  # AMD
-                eprint("detected amd gpu")
+                print_g("detected amd gpu")
                 self._handle_amd(device, pkgs)
+            else:
+                print_e(f"unknown vendor {vendor}")
 
         return pkgs
 
@@ -82,16 +101,16 @@ class HardwareDetector:
 
         if device_id.startswith(ancient_prefixes):
             # aint doing this, do it manually
-            eprint("unknown (unsupported?) intel gpu")
+            print_e("unknown (unsupported?) intel gpu")
             return
         elif device_id.startswith(legacy_prefixes):
-            eprint("detected legacy intel gpu")
+            print_g("detected legacy intel gpu")
             pkgs.add("vulkan-intel")
             pkgs.add("libva-intel-driver")
             pkgs.add("libvdpau-va-gl")
         else:
             # modern (gen 8+) -> ihd driver
-            eprint("detected modern intel gpu")
+            print_g("detected modern intel gpu")
             pkgs.add("intel-media-driver")
             pkgs.add("vulkan-intel")
             pkgs.add("libvpl")
@@ -109,20 +128,20 @@ class HardwareDetector:
         # prefixes: 1e, 1f, 20, 21 (16xx), 22+ (ampere/ada/blackwell)
         # note: gtx 1660 ti is 0x2182
         if device_id.startswith(("1e", "1f")) or device_id[0] != "1":
-            eprint("detected modern nvidia gpu")
+            print_g("detected modern nvidia gpu")
             pkgs.add("nvidia-open")
 
         # MAXWELL (9xx) & PASCAL (10xx) -> Proprietary
         # maxwell: 13, 14, 16, 17
         # pascal: 15 (gp100), 1b, 1c, 1d
         elif device_id.startswith(("13", "14", "15", "16", "17", "1b", "1c", "1d")):
-            eprint("detected legacy nvidia gpu")
+            print_g("detected legacy nvidia gpu")
             pkgs.add("nvidia")
 
         # KEPLER (6xx/7xx) & OLDER -> Legacy (Ignored)
         # IDs: 0*, 10, 11, 12 are ignored.
         else:
-            eprint("unknown (ancient?) nvidia gpu")
+            print_e("unknown (ancient?) nvidia gpu")
 
     def _handle_amd(self, device_id: str, pkgs: set):
         pkgs.add("vulkan-radeon")
